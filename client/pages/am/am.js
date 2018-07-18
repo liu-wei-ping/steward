6332ab92ce5cca4c89bf09c2ab6212f5a3681da8
lwp <liu_weipinglove@163.com>
2018-07-19 02:41:27 +0800

    首页样式

--- client/pages/am/am.js
+++ client/pages/am/am.js
@@ -1,66 +1,72 @@
 // pages/am/am.js
+var util = require('../../utils/util.js')
+var timer;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    _num: 1,
+    _dateTimer: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function(options) {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
-  },
+  onUnload: function() {
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
+  },
+  headClick: function(e) {
+    var num = e.currentTarget.dataset.num;
+    this.setData({
+      _num: num
+    })
+  },
+  startClick: function(e) {
+    this.timing(this);
+  },
+  timing:function(that) {
+    timer = setTimeout(function() {
+      that.setData({
+        _dateTimer: util.formatUnixTime(Date.parse(new Date()))
+      })
+      that.timing(that)
+    }, 1000);
   }
 })
--- client/utils/util.js
+++ client/utils/util.js
@@ -8,7 +8,26 @@ const formatTime = date => {
 
   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
+const formatUnixTime = (time, format) => {
+  format = format == undefined ? "Y-M-D:h:m:s" : format;
+  var formateArr = ["Y", "M", "D", "h", "m", "s"];
+  var returnArr = [];
+  console.log(time);
+  var timestamp = Date.parse(new Date());
+  var date = new Date(time);
+  returnArr.push(date.getFullYear());
+  returnArr.push(formatNumber(date.getMonth() + 1));
+  returnArr.push(formatNumber(date.getDate()));
 
+  returnArr.push(formatNumber(date.getHours()));
+  returnArr.push(formatNumber(date.getMinutes()));
+  returnArr.push(formatNumber(date.getSeconds()));
+
+  for (var i in returnArr) {
+    format = format.replace(formateArr[i], returnArr[i]);
+  }
+  return format;
+};
 const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
@@ -17,26 +36,32 @@ const formatNumber = n => {
 
 // 显示繁忙提示
 var showBusy = text => wx.showToast({
-    title: text,
-    icon: 'loading',
-    duration: 10000
+  title: text,
+  icon: 'loading',
+  duration: 10000
 })
 
 // 显示成功提示
 var showSuccess = text => wx.showToast({
-    title: text,
-    icon: 'success'
+  title: text,
+  icon: 'success'
 })
 
 // 显示失败提示
 var showModel = (title, content) => {
-    wx.hideToast();
+  wx.hideToast();
 
-    wx.showModal({
-        title,
-        content: JSON.stringify(content),
-        showCancel: false
-    })
+  wx.showModal({
+    title,
+    content: JSON.stringify(content),
+    showCancel: false
+  })
 }
 
-module.exports = { formatTime, showBusy, showSuccess, showModel }
+module.exports = {
+  formatTime,
+  formatUnixTime,
+  showBusy,
+  showSuccess,
+  showModel
+}
